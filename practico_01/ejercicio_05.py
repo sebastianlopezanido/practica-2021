468b605a8e111dd76488f14630a93901a82358aa
Lopeza <k43714@frro.utn.edu.ar>
2021-03-30 23:07:07 -0300

    resolucion ejercicio 5 y 6
    falta el ultimo challenge opcional del ejercicio 6

--- practico_01/ejercicio_05.py
+++ practico_01/ejercicio_05.py
@@ -9,7 +9,20 @@ def multiplicar_basico(numeros: Iterable[float]) -> float:
 
     Restricciones: No usar bibliotecas auxiliares (Numpy, math, pandas).
     """
-    pass # Completar
+   
+    product = 1.0               #inicializo la variable que va a dcontener el producto de todos los numeros, arranca en 1 
+    count = 0                   #inicializo la variable que va a contar la cantidad de elementos de la lista, arranca en 0
+    for i in numeros:           #para cada elemento "i" en numeros
+        product = product * i   #el producto es igual al producto por el numero "i"
+        count =+ 1              #la cantidad aumenta 1 en 1
+    if count == 0:              #si no hay elementos devuelvo 0
+        return 0
+    return product              #sino devuelvo el producto
+    
+
+    
+        
+
 
 
 # NO MODIFICAR - INICIO
@@ -31,7 +44,19 @@ def multiplicar_reduce(numeros: Iterable[float]) -> float:
     """CHALLENGE OPCIONAL - Re-escribir utilizando reduce.
     Referencia: https://docs.python.org/3.8/library/functools.html#functools.reduce
     """
-    pass # Completar
+    def counting(a, b):                 #funcion usada para contar con reduce
+        return a + 1    
+
+    def multiply (a, b):                #funcion usada para multiplicar con reduce
+        return a * b
+    
+    count = reduce(counting, numeros,0) #llamo al reduce con la funcion counting, la lista numeros, que se inicialice en 0 y guardo el resultado en count
+    if count == 0:                      #si count es 0 devuelvo 0
+        return 0    
+                                         #sino
+    product = reduce(multiply, numeros,1)#llamo al reduce con la funcion multiply , la lista numeros, que se inicialice en 1 y guardo el resultado en product
+    return product                       #devuelvo el producto
+    
 
 
 # NO MODIFICAR - INICIO
--- practico_01/ejercicio_06.py
+++ practico_01/ejercicio_06.py
@@ -7,7 +7,18 @@ def numeros_al_final_basico(lista: List[Union[float, str]]) -> List[Union[float,
     """Toma una lista de enteros y strings y devuelve una lista con todos los
     elementos numéricos al final.
     """
-    pass # Completar
+    listaEnteros = []                           #inicializo lista enteros
+    listaStrings = []                           #inicializo lista strings
+
+    for i in lista:                             #para cada elemento de la lista
+        if (isinstance(i, int) ):               #compruebo que el tipo del elemento sea int
+            listaEnteros.append(i)              #si es int, lo agrego en listaEnteros
+        else:                                   #sino
+            listaStrings.append(i)              #lo agrego en listaString
+    
+    
+    listaUnida = listaStrings + listaEnteros    #uno las listas
+    return listaUnida                           #devuelvo la listaUnida
 
 
 # NO MODIFICAR - INICIO
@@ -20,7 +31,15 @@ assert numeros_al_final_basico([3, "a", 1, "b", 10, "j"]) == ["a", "b", "j", 3, 
 
 def numeros_al_final_comprension(lista: List[Union[float, str]]) -> List[Union[float, str]]:
     """Re-escribir utilizando comprensión de listas."""
-    pass # Completar
+
+    listaEnteros = [int(i) for i in lista if isinstance(i, int)]  #genero una nueva lista de compresion con solo los enteros
+    listaStrings = [i for i in lista if isinstance(i, str)]       #genero una nueva lista de compresion con solo los strings
+
+    listaUnida = []                                               #instancia nueva lista
+    listaUnida.extend(listaStrings)                               #le agrego la lista string
+    listaUnida.extend(listaEnteros)                               #le agrego la lista de enteros
+
+    return  listaUnida                                            #devuelvo la listaUnida
 
 
 # NO MODIFICAR - INICIO
@@ -35,8 +54,11 @@ def numeros_al_final_sorted(lista: List[Union[float, str]]) -> List[Union[float,
     """Re-escribir utilizando la función sorted con una custom key.
     Referencia: https://docs.python.org/3/library/functions.html#sorted
     """
-    pass # Completar
+    def isint(i: Union[float, str]):    #defino funcion isint para usarla como key y customizar la funcion sorted
+        return isinstance(i, int)       #la funcion recibe un elemento y devuelve true si es int, false si no es
 
+    return sorted(lista, key = isint)   #ordeno la lista con la key customizada(), me va ordenar la lista con los numeros al final
+     
 
 # NO MODIFICAR - INICIO
 assert numeros_al_final_sorted([3, "a", 1, "b", 10, "j"]) == ["a", "b", "j", 3, 1, 10]
@@ -50,8 +72,20 @@ def numeros_al_final_filter(lista: List[Union[float, str]]) -> List[Union[float,
     """CHALLENGE OPCIONAL - Re-escribir utilizando la función filter.
     Referencia: https://docs.python.org/3/library/functions.html#filter
     """
-    pass # Completar
+    def isint(i):                      # Primero declaramos una función condicional
+       return  isinstance(i, int)      # Sólo devolvemos True si el elemento es int
+    def isstr(i):                      # Primero declaramos una función condicional
+       return  isinstance(i, str)      #Sólo devolvemos True si el elemento es str
 
+
+    
+    listaEnteros = filter(isint, lista)  #genero una nueva lista filtrada con solo los enteros
+    listaStrings = filter(isstr, lista)  #genero una nueva lista fitlrada con solo los strings
+
+    listaUnida = []                       #instancia nueva lista
+    listaUnida.extend(listaStrings)       #le agrego la lista string
+    listaUnida.extend(listaEnteros)       #le agrego la lista de enteros
+    return listaUnida                     #devuelvo la listaUnida
 
 # NO MODIFICAR - INICIO
 if __name__ == "__main__":
